60d55ff7892a506563194a299d9c51e27c7d33c2
Folarin <blfolarin@gmail.com>
2017-03-01 10:19:53 -0500

    first commit

---
+++ tsf/learner.py
@@ -0,0 +1,35 @@
+import numpy as np
+import scipy as sp 
+
+class StatLearner:
+	def __init__(self):
+		pass
+
+	@staticmethod
+	def previousRegresser(x):
+		pass
+
+	@staticmethod
+	def linearRegresser(x):
+		pass
+
+	@staticmethod
+	def aRCH(x,index=(1,1)):
+		pass
+
+	@staticmethod
+	def gARCH(x,index=(1,1)):
+		pass
+
+	@staticmethod
+	def neuralNet(x,network):
+		pass
+
+	@staticmethod
+	def logisticRegresser(x):
+		pass
+
+	@classmethod
+	def train(self, x):
+		pass
+
