65794014537fd90a24657cfba28da1aff66ebe22
Folarin <blfolarin@gmail.com>
2017-03-02 09:32:12 -0500

    implementation of timeseriesdf

---
+++ tsf/timeseries.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+class TimeSeriesDF:
+	def __init__(self,filename="../data/AUDUSD.cv"):
+		#self.df = pd.read_csv(filename)
+		pass
+
+	def spread(self):
+		pass
+
+
+if __name__ == "__main__":
+	ts = TimeSeriesDF()
