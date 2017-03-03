0768b218e9b1dcbb0cbc5a3324db8c9faabbcb26
Folarin <blfolarin@gmail.com>
2017-03-03 12:43:20 -0500

    implementing constructor

--- tsf/timeseries.py
+++ tsf/timeseries.py
@@ -1,13 +1,15 @@
 import pandas as pd
 
 class TimeSeriesDF:
-	def __init__(self,filename="../data/AUDUSD.cv"):
-		#self.df = pd.read_csv(filename)
-		pass
+	def __init__(self,filename="../data/AUDUSD.csv"):
+		self.df = pd.read_csv(filename)
+		dt = self.df.Date.astype(str).str.cat(self.df.Time.astype(str), sep=' ')
+		pd.to_datetime(dt)
+		self.df.set_index(pd.Series(dt),inplace=True)
 
 	def spread(self):
 		pass
 
 
 if __name__ == "__main__":
-	ts = TimeSeriesDF()
+	tsdf = TimeSeriesDF()
